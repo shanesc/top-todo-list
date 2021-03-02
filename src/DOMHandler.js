4687c631ef2e41746d7dd640fea73a9e5f7e98e9
Shane Mitchell <shaneSC@gmail.com>
2021-03-02 18:37:36 +0000

    feat: add DOM methods for adding and deleting Todo items

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -1,11 +1,43 @@
 export default {
-  addTodo(todo) {
-    const list = document.querySelector('.todo-list');
-
+  addItem(item) {
+    const todoList = document.querySelector('.todo-list');
     const el = document.createElement('li');
     el.classList.add('todo');
-    el.textContent = todo.title;
+    el.id = item.id;
 
-    list.appendChild(el);
+    const title = document.createElement('span');
+    title.classList.add('todo__title');
+    title.textContent = item.title;
+
+    const removeEl = document.createElement('button');
+    removeEl.classList.add('btn-remove');
+    removeEl.textContent = 'X';
+    removeEl.addEventListener('click', () => {
+      this.removeItem.call(el);
+    });
+
+    el.appendChild(removeEl);
+    el.appendChild(title);
+    todoList.appendChild(el);
+  },
+  // renderList(list) {
+  //   this.clearList();
+  //   const todoList = document.querySelector('.todo-list');
+  //   list.forEach((todo) => {
+  //     const el = document.createElement('li');
+  //     el.classList.add('todo');
+  //     el.id = todo.id;
+  //     el.textContent = todo.title;
+  //     todoList.appendChild(el);
+  //   });
+  // },
+  // clearList() {
+  //   const todoList = document.querySelector('.todo-list');
+  //   while (todoList.hasChildNodes()) {
+  //     todoList.removeChild(todoList.lastChild);
+  //   }
+  // },
+  removeItem() {
+    this.remove();
   },
 };
