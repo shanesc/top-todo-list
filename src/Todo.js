ce85b1b92e022b3d5843c7ed50cc192e20382d0f
Shane Mitchell <shanesc@gmail.com>
2021-03-11 14:18:03 +0000

    feat: add priority property to Todo class

--- src/Todo.js
+++ src/Todo.js
@@ -8,9 +8,10 @@
 import { v4 as uuidv4 } from 'uuid';
 
 class Todo {
-  constructor(title) {
-    this.title = title;
+  constructor({ title, priority = 4 }) {
     this.id = uuidv4();
+    this.title = title;
+    this.priority = priority;
   }
 }
 
